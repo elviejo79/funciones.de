8c45f4e9c897c72db13a6b66e1d75aa9866a6dbd
elviejo79 <agarciafdz@gmail.com>
2013-12-07 13:50:53 -0600

    hace el crawler de Cinepolis... necesitamos que no haga request por cada cine

--- scrapping/cinepolis/cinepolis.go
+++ scrapping/cinepolis/cinepolis.go
@@ -4,10 +4,21 @@ import(
 	"io/ioutil"
 	"fmt"
 	"github.com/moovweb/gokogiri"
-	//	"github.com/gregjones/httpcache"
+	"github.com/moovweb/gokogiri/xml"
 	"net/http"
 	"strconv"
+	"strings"
+	"time"
 )
+
+type Showtime struct{
+	IdTheater string
+	IdMovie string
+	Language string
+	RoomType string
+	Date string
+	Time string
+}
 
 func ExtractCities(url string) map[int]string {
 
@@ -31,6 +42,105 @@ func ExtractCities(url string) map[int]string {
 
 }
 
+func ExtractTheaters(url string) map[int]string {
+	result := make(map[int]string)
+	html,err := getBody(url)
+	if err != nil {
+		fmt.Printf("%#v",err)
+	}
+	doc, _ := gokogiri.ParseHtml(html)
+	defer doc.Free()
+	//theaters,_ := doc.Search("//a[ends-with(@id,'306')]");
+	movies,_ := doc.Search("//a[contains(@id, 'idPelCine')]")
+	for _, m := range movies{ 
+		cineId := nodeContent("@id",m)[14:]
+		cineName := nodeContent("//select[@name='cartelera"+cineId+"']/parent::*/parent::*/parent::*//span[@class='TitulosBlanco']",m)
+		intCineId, _ := strconv.Atoi(cineId)
+		result[intCineId] = cineName
+	}
+	return result
+}
+
+func ExtractMovies(url string, idCine int) (res []Showtime, err error) {
+	html,err := getBody(url)
+	if err != nil {
+		fmt.Printf("%#v",err)
+	}
+	doc, _ := gokogiri.ParseHtml(html)
+	defer doc.Free()
+	//theaters,_ := doc.Search("//a[ends-with(@id,'306')]");
+	xpath := fmt.Sprintf("//a[contains(@id, 'idPelCine') and (substring(@id, string-length(@id) -2)=%d)]",idCine)
+	movies,_ := doc.Search(xpath)
+	fmt.Printf("url: %s \nXpath: %s \nExtractMovies: %#v \n\n", url,xpath,movies)
+	for _, m := range movies{ 
+		cineId := nodeContent("@id",m)[14:]
+		titulo := nodeContent("parent::*//a[@class='peliculaCartelera']",m)
+		subtitulos := titulo[len(titulo)-3:]
+		if subtitulos == "Sub" {
+			subtitulos = "SUBTITULADA"
+                } else {
+			subtitulos = "ESPAÑOL"
+		}
+
+		sala := titulo[len(titulo)-7:len(titulo)-4]
+		if strings.Contains(titulo," 4D") {
+			titulo=titulo[:strings.Index(titulo," 4D")]
+			sala = "4D"
+		} else if strings.Contains(titulo," 3D ") {
+			titulo=titulo[:strings.Index(titulo," 3D ")]
+			sala = "3D"
+		} else if strings.Contains(titulo," IMAX") {
+			titulo=titulo[:strings.Index(titulo," IMAX")]
+			sala = "IMAX"
+		} else if strings.Contains(titulo," XE") {
+			titulo=titulo[:strings.Index(titulo," XE ")]
+			sala = "XE"
+		} else if strings.Contains(titulo," Dig ") {
+			titulo=titulo[:strings.Index(titulo," Dig ")]
+			sala = "Dig"
+		} else {
+			//titulo=titulo
+			sala = ""
+		}
+		sala = "R"+sala
+		titulo = strings.ToUpper(titulo)
+		titulo = strings.Replace(titulo,":","", -1)
+		t := time.Now().Format("20060102")
+		
+		row := Showtime{
+			cineId, //cineID
+			titulo,
+			subtitulos,
+			sala,
+			t,
+			"00:00",
+		}
+		
+
+		hours,err := m.Search("parent::*/parent::*//*[contains(@class,'horariosCartelera')]")
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		horas := []string{}
+		for _,e := range hours {
+			t,_:= time.Parse("3:04pm",e.Content())
+			horas = append(horas,t.Format("15:04"))
+		}
+
+		//if row != nil {
+			for _,h := range horas {
+				row.Time = h
+				res = append(res,row)
+			}
+		//}
+
+	}
+
+	return
+}
+
+
 func getBody(url string) (body []byte, err error) {
 	client := http.Client{}
 	resp, err := client.Get(url)
@@ -44,3 +154,12 @@ func getBody(url string) (body []byte, err error) {
         }
 	return body,nil
 }
+
+func nodeContent(x_path string,m xml.Node) (result string){
+	ts,_ := m.Search(x_path)
+	for _,e := range ts{
+		result = e.Content()
+	}
+	return 
+}
+
--- scrapping/crawler.go
+++ scrapping/crawler.go
@@ -1,10 +1,161 @@
 package main
 
 import (
-	"fmt"
 	"./cinepolis"
+	"fmt"
+	"github.com/jmcvetta/neoism"
+	"log"
 )
 
+func cleanup(db *neoism.Database) {
+	qs := []*neoism.CypherQuery{
+		&neoism.CypherQuery{
+			Statement: `START r=rel(*) DELETE r`,
+		},
+		&neoism.CypherQuery{
+			Statement: `START n=node(*) DELETE n`,
+		},
+	}
+	err := db.CypherBatch(qs)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
 func main() {
-	fmt.Printf("%#v\n", cinepolis.ExtractCities("http://cinepolis.com"))
+
+	root, err := neoism.Connect("http://localhost:7474/db/data")
+	if err != nil {
+		log.Fatal("failed to open default database", err)
+	}
+
+	cleanup(root)
+
+	company, _ := root.CreateNode(map[string]interface{}{"url": "http://cinepolis.com", "name": "cinepolis"})
+	company.AddLabel("Company")
+
+	cities := cinepolis.ExtractCities("http://cinepolis.com")
+	var newCity *neoism.Node
+	for i, c := range cities {
+		newCity, err = root.CreateNode(map[string]interface{}{"idCity": i, "name": c})
+		if err != nil {
+			log.Fatal("failed to create node:", err)
+		}
+		newCity.AddLabel("City")
+		_, err = company.Relate("operates_in", newCity.Id(), neoism.Props{})
+		if err != nil {
+			log.Fatal("failed to create relationship:", err)
+		}
+
+		CreateTheaters(root, newCity)
+
+	}
+
+
+}
+
+type Movie struct {
+	NeoID int    `json:"neoId"`
+	Title string `json:"n.title"`
+}
+
+///////////////////////////////////////// anidados
+func CreateTheaters(root *neoism.Database, newCity *neoism.Node) {
+	properties, _ := newCity.Properties()
+	fmt.Printf("%#v \n", properties)
+	urlTheater := fmt.Sprintf("http://cinepolis.com/_CARTELERA/cartelera.aspx?ic=%d", int(properties["idCity"].(float64)))
+	theaters := cinepolis.ExtractTheaters(urlTheater)
+
+	for it, t := range theaters {
+		newTheater, err := root.CreateNode(map[string]interface{}{"idTheater": it, "name": t})
+		if err != nil {
+			log.Fatal("failed to create Theater:", err)
+		}
+		newTheater.AddLabel("Theater")
+		newCity.Relate("has_a", newTheater.Id(), neoism.Props{})
+		
+		CreateShowtimes(root, newCity, newTheater)
+	}
+
+
+}
+
+func CreateShowtimes (root *neoism.Database, newCity *neoism.Node, newTheater *neoism.Node) {
+	properties, _ := newCity.Properties()
+	fmt.Printf("%#v \n", properties)
+	urlTheater := fmt.Sprintf("http://cinepolis.com/_CARTELERA/cartelera.aspx?ic=%d", int(properties["idCity"].(float64)))
+
+	propertiesT, _ := newTheater.Properties()
+	showtimes, err := cinepolis.ExtractMovies(urlTheater, int(propertiesT["idTheater"].(float64)))
+	fmt.Printf("Showtimes: %#v \n", showtimes)
+	if err != nil {
+		log.Fatal("Problemas al sacar las funciones de cinepolis:", err)
+	}
+	for _, s := range showtimes {
+		newNode, err := root.CreateNode(map[string]interface{}{
+			"IdTheater": s.IdTheater,
+			"IdMovie":   s.IdMovie,
+			"Language":  s.Language,
+			"RoomType":  s.RoomType,
+			"Date":      s.Date,
+			"Time":      s.Time,
+		})
+
+		if err != nil {
+			log.Fatal("failed to create Showtime:", err)
+		}
+
+		newNode.AddLabel("Showtime")
+		newTheater.Relate("performs_at", newNode.Id(), neoism.Props{})
+		movieID, err := GetMovieID(root, s.IdMovie)
+		if err != nil {
+			movieID, _ = CreateMovie(root, s.IdMovie, "Movie")
+		}
+		newNode.Relate("presents", movieID, neoism.Props{})
+
+	}
+
+}
+
+func CreateMovie(db *neoism.Database, title string, label string) (int, error) {
+	newNode, err := db.CreateNode(map[string]interface{}{
+		"title": title,
+	})
+
+	if err != nil {
+		log.Fatal("failed to create %s:", label, err)
+		return 0, err
+	}
+	newNode.AddLabel(label)
+	return newNode.Id(), nil
+}
+
+//gets the id of a movie
+func GetMovieID(db *neoism.Database, title string) (movieID int, err error) {
+	//
+	// Query with integer parameter and string results
+	//
+	var result []Movie
+	cq := neoism.CypherQuery{
+		Statement: "MATCH (n:Movie) where n.title = '" + title + "'  RETURN ID(n) as neoId, n.title",
+		Parameters: map[string]interface{}{
+			"title": title,
+		},
+		Result: &result,
+	}
+
+
+	err = db.Cypher(&cq)
+	if err != nil {
+		fmt.Printf("%#v", err)
+		log.Fatal(err)
+	}
+
+	if len(result) > 0 {
+		movieID = result[0].NeoID
+	} else {
+		err = fmt.Errorf("movie with title %s not found", title)
+	}
+
+	return
 }
